742f9984dce0be43c4484c09bfd65257152c6411
Norbert Tasarz <tasarz_norbert@wp.pl>
2023-06-05 12:24:19 +0200

    sorting and some small changes

--- Main.cpp
+++ Main.cpp
@@ -6,8 +6,9 @@ int main()
 {
 
     std::vector<std::pair<std::string, std::pair<int, int>>> allStations = GetAllStations();
-    std::vector<std::pair<int, int>> firstStation = GetOneStationCoords("Ostrów Wielkopolski", allStations);
-    std::vector<std::pair<int, int>> secondStation = GetOneStationCoords("Mońki", allStations);
+    std::vector<std::pair<int, int>> firstStation = GetOneStationCoords("Warszawa", allStations);
+    std::vector<std::pair<int, int>> secondStation = GetOneStationCoords("Gdańsk", allStations);
+    // 2 is best for now i think
     const int ODCHYLENIE = 2; // --- Change if Path is to wonky maybe will find better way later to do this
     std::set<std::pair<int, int>> sortedStations = SortStationsByDistance(GetStationsFromArea(allStations, getPointsBetweenTwoStations(firstStation, secondStation),ODCHYLENIE), firstStation, secondStation);
     DrawMap(allStations, GetPathBetweenMultipleStations(sortedStations));
@@ -15,6 +16,7 @@ int main()
     // sort stations so that path is more logical maybe or something so it looks more natural - done
     // maybe do so that next station cannot me equeal or less distance to endstation - idk
     // change sp that stations cannot go aboxe, below or behind end stations 
+    // add function that generates path between given stations in between first nad last station
     // check for more edge cases and bugs
 
     return 0;
